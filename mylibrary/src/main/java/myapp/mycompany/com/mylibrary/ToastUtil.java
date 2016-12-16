d25f6f9c50e01c9e04c053031d943b0583d5b8d2
yanjingming <727787745@qq.com>
2016-12-17 00:00:14 +0800

    w

---
+++ mylibrary/src/main/java/myapp/mycompany/com/mylibrary/ToastUtil.java
@@ -0,0 +1,25 @@
+package myapp.mycompany.com.mylibrary;
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * 吐司工具类
+ *
+ * @author yjm
+ * @date 16/12/16
+ */
+
+/**
+ * 显示toast
+ *
+ * @param context
+ * @param str
+ */
+public class ToastUtil
+{
+
+		public static void showToast(Context context, String str) {
+				Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+		}
+}
+
